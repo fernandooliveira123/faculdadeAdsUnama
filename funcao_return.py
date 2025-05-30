0eae8fd5f25374b4e81bdc8ef79b2ab58eda3195
Fernando Oliveira <fernando_ibp@hotmail.com>
2025-05-30 20:59:12 +0000

    funcao e return

---
+++ funcao_return.py
@@ -0,0 +1,32 @@
+#def soma(a, b)
+   #s = a + b
+  # return s 
+
+  # n1  = int(input("número1")
+
+
+def dobro(num):
+    d = num *2 
+    return d 
+x = int(input ("insira um número "))
+resul = dobro(x)
+print(f"o dobro de {x} é {resul}")
+
+def = conversor(ccelsius):
+    f = celsius * (9/5) + 32 
+    return f 
+tc = float(input("insira um número "))
+tf = conversor (tc)
+print (f"{tc} Cequivale a {tf}tf)
+
+
+def maior(a,b):
+  if a>b:
+  return a
+  else:
+  return b 
+
+  numA= int(input("insira um número: "))
+  numB = int(input("Insira número")
+    m= maior (numA, nnumB
+print(f"entre {numA} e {nunumb} o maior é, m )
