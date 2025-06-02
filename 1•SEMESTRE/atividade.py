461f54dd53a3e8422a9b32f48e94f50f7f97f75c
Fernando Oliveira <fernando_ibp@hotmail.com>
2025-06-02 20:45:25 -0300

    atividade_2

--- 1•SEMESTRE/atividade.py
+++ 1•SEMESTRE/atividade.py
@@ -5,10 +5,10 @@
 
 # 2. Crie um programa que solicite dois números ao usuário, some-os e exiba o resultado.
 
-numero1 = float(input("Insira um numero"))
-numero2 =float(input("Insira mais um numero"))
-resultado = numero1 + numero2
-print("Seu numero 1 com o numero 2 somados gera o resultado", resultado)
+# numero1 = float(input("Insira um numero"))
+# numero2 =float(input("Insira mais um numero"))
+# resultado = numero1 + numero2
+# print("Seu numero 1 com o numero 2 somados gera o resultado", resultado)
 
 # 4. Escreva um programa que leia um número e exiba o seu dobro e triplo
 
@@ -16,10 +16,10 @@ numero = int(input("Me mande um numero =D"))
 dobro = numero * 2
 triplo = numero * 3
 print ("o dobro do numero é:", dobro)
-print ("o dobro do numero é:", triplo)
+print ("o triplo do numero é:", triplo)
 
 # 5. Crie um programa que peça a idade do usuário e exiba quantos dias ele já viveu (considerando que um ano tem 365 dias)
 
-idade = int(input("Olá qual a sua idade?"))
-diasVividos = idade * 365
-print("Você já viveu aproximadamente:", diasVividos,  "dias, parabéns que venham muitas mais dias de vida!")
+# idade = int(input("Olá qual a sua idade?"))
+# diasVividos = idade * 365
+# print("Você já viveu aproximadamente:", diasVividos,  "dias, parabéns que venham muitas mais dias de vida!")
--- 1•SEMESTRE/aula5_estrutura_de_repeticao.py
+++ 1•SEMESTRE/aula5_estrutura_de_repeticao.py
@@ -1,1 +1,12 @@
-#
+#Crie um programa realize uma contagem regressiva em tela, começando em 10 até 1 e no final exiba a mensagem “FIM!”
+
+# for i in range(10,0,-1):
+#     print(i)
+# print("FIM")
+
+#Crie um programa que receba 10 notas de alunos e calcule a média aritmética delas
+for i in range(1,11):
+    nota = float(input("Insira sua nota: "))
+    media = nota / 10
+print (f"sua média é, {media}")
+
