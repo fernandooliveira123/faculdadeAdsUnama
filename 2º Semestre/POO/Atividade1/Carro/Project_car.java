c4b84c550830e8ff5bf54bed5666535546c9aeb4
Fernando Oliveira <fernando_ibp@hotmail.com>
2025-08-25 20:54:51 -0300

    Create Project_car.java

---
+++ 2º Semestre/POO/Atividade1/Carro/Project_car.java
@@ -0,0 +1,22 @@
+public class ProjetoCarro {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro();
+        carro1.marca = "Toyota";
+        carro1.modelo = "Corolla";
+        carro1.ano = 2024;
+
+        Carro carro2 = new Carro();
+        carro2.marca = "Honda";
+        carro2.modelo = "Civic";
+        carro2.ano = 2015;
+
+        carro2.modelo = "Civic Turbo";
+        carro2.ano = 2023;
+
+        carro1.carrodados();
+        carro1.ligar();
+
+        carro2.carrodados();
+        carro2.ligar();
+    }
+}
