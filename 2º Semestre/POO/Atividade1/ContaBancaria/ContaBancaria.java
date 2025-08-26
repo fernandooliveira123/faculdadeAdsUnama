6946a2b2072cf20c14198d6245588366c0142f88
Fernando Oliveira <fernando_ibp@hotmail.com>
2025-08-25 21:07:32 -0300

    Create ContaBancaria.java

---
+++ 2º Semestre/POO/Atividade1/ContaBancaria/ContaBancaria.java
@@ -0,0 +1,23 @@
+public class ContaBancaria{
+    public String numeroConta;
+    public String titular;
+    public double saldo;
+
+    public void depositar(double valor) {
+        saldo += valor;
+        System.out.println("Depósito de R$ " + valor + " realizado.");
+    }
+
+    public void sacar(double valor) {
+        if (saldo >= valor) {
+            saldo -= valor;
+            System.out.println("Saque de R$ " + valor + " realizado.");
+        } else {
+            System.out.println("Saldo insuficiente.");
+        }
+    }
+
+    public void consultarSaldo() {
+        System.out.println("Saldo atual: R$ " + saldo);
+    }
+}
