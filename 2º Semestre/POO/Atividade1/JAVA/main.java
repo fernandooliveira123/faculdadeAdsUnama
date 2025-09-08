10282bfa3b75f2036f2dba4457503cc57254c33b
Fernando Oliveira <fernando_ibp@hotmail.com>
2025-09-08 16:42:00 -0300

    primeiro codigo java

---
+++ 2º Semestre/POO/Atividade1/JAVA/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main (String [] args) {
+        int idade = 31;
+        int nota = 10;
+        
+        System.out.println("Sua idade é: " + idade + " Sua nota é " + nota);
+    }
+}
