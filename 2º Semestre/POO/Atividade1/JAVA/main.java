96d072aa19637cb4b4bcd1d26b6ae6e989de5569
Fernando Oliveira <fernando_ibp@hotmail.com>
2025-09-09 16:40:44 -0300

    Java com adição de algumas variaveis

--- 2º Semestre/POO/Atividade1/JAVA/main.java
+++ 2º Semestre/POO/Atividade1/JAVA/main.java
@@ -1,8 +1,11 @@
-public class main {
+public class Main {
     public static void main (String [] args) {
+        String nome = "Fernando";
         int idade = 31;
         int nota = 10;
+        String frase = "Tenho uma visão e estou em busca dela!";
         
-        System.out.println("Sua idade é: " + idade + " Sua nota é " + nota);
+        System.out.println("Meu nome é: "  + nome + " Sua idade é: " + idade + " Sua nota é " + nota);
+        System.out.println(frase);
     }
 }
